49844568034dbc28521058997edb59139fafb64b
Nathianny <nathianny_preira26@hotmail.com>
2023-07-08 14:35:58 -0300

    Conteúdo de Operadores Aritméticos

---
+++ Conteúdos/estruturas_operadores/operadores_aritmeticos.py
@@ -0,0 +1,22 @@
+"""
+Operadores Aritméticos
+"""
+
+# Criação de variáveis
+a = 10;
+b = 3;
+
+soma = a + b; # Somando valores
+subtracao = a - b; # Subtraindo valores
+multiplicacao = a * b; # Multiplicando valores
+divisao = a / b; # Dividindo valores
+resto = a % b; # Resto da divisão dos valores
+descarteParteFracionaria = a // b; # Descartando parte fracionária dos valores
+potencia = a ** b; # Potenciação dos valores
+print("A soma dos números é:", soma);
+print("A subtração dos números é:", subtracao);
+print("A multiplicação dos números é:", multiplicacao);
+print("A divisão dos números é:", divisao);
+print("A potenciação dos números é:", potencia);
+print("O resto da divisão é:", resto);
+print("Divisão após descartar parte fracionária:", descarteParteFracionaria);
