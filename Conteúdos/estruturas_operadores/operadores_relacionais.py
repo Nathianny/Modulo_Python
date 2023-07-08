556135ec4efca757cc5854a9113b8b6f33e3d878
Nathianny <nathianny_preira26@hotmail.com>
2023-07-08 14:33:13 -0300

    conteúdo de operadores relacionais

---
+++ Conteúdos/estruturas_operadores/operadores_relacionais.py
@@ -0,0 +1,37 @@
+"""
+Operadores Relacionais
+== - igual
+!= - diferente
+<  - menor
+>  - maior
+<= - menor ou igual
+>= - maior ou igual
+"""
+
+# Criação de variáveis
+x = 5;
+y = 7;
+
+# Maior que
+print(x > y); # Esperamos False
+print();
+
+# Menor que
+print(x < y); # Esperamos True
+print();
+
+# Igualdade
+print(x == y); # Esperamos False
+print();
+
+# Maior ou igual a
+print(x >= y); # Esperamos False
+print();
+
+# Menor ou iual a 
+print(x <= y); # Esperamos True
+print();
+
+# Diferente de
+print(x != y); # Esperamos True
+print();
