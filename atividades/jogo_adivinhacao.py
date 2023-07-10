b928d5a7318a3a5a67880fd36ef021b8ea7ce963
Nathianny <nathianny_preira26@hotmail.com>
2023-07-10 20:16:29 -0300

    início do jogo de adivinhação

--- atividades/jogo_adivinhacao.py
+++ atividades/jogo_adivinhacao.py
@@ -3,6 +3,29 @@
 
 # Proposta
 # Melhorias na versão inicial do jogo
-'''- Adicionar limite máximo de tentativas
-   - Permitir que o jogador escolha o intervalo de números
-   - Incluir uma opção para o jogador jogar novamente ou sair do jogo após acertar ou esgotar todas as tentativas.'''
+# - Adicionar limite máximo de tentativas
+# - Permitir que o jogador escolha o intervalo de números
+# - Incluir uma opção para o jogador jogar novamente ou sair do jogo após acertar ou esgotar todas as tentativas.'''
+   
+import random
+
+def adivinhacao():
+    numero_secreto = random.randint(1, 100)  # Gera um número aleatório entre 1 e 100
+    tentativas = 0
+
+    print("Bem-vindo ao jogo de adivinhação de números!")
+    print("Estou pensando em um número entre 1 e 100.")
+
+    while True:
+        palpite = int(input("Digite o seu palpite: "))
+        tentativas += 1
+
+        if palpite < numero_secreto:
+            print("Tente um número maior!")
+        elif palpite > numero_secreto:
+            print("Tente um número menor!")
+        else:
+            print(f"Parabéns! Você acertou o número em {tentativas} tentativas!")
+            break
+
+adivinhacao()
